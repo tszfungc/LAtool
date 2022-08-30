3e83c58f8ea373f7a4ab18d3cf4deee80b17d332
tszfungc <tszfungc@usc.edu>
2022-08-30 01:19:11 -0700

    Add read msprime tree sequence

--- src/latool/io/__init__.py
+++ src/latool/io/__init__.py
@@ -1,8 +1,10 @@
 from .pgen_write import write_pgen
 from .rfmix_read import read_rfmix_fb, read_rfmix_msp
+from .ts_read import read_msp_ts
 
 __all__ = [
     "read_rfmix_fb",
     "read_rfmix_msp",
     "write_pgen",
+    "read_msp_ts",
 ]
