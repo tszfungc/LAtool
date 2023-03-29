13a0f0954f574ae708ea8c3c66100740e5f62bed
tszfungc <tszfungc@usc.edu>
2023-03-29 14:22:00 -0700

    Add rfmix fb tsv output to io

--- src/latool/io/__init__.py
+++ src/latool/io/__init__.py
@@ -1,6 +1,6 @@
 from .pgen_write import write_pgen
 from .rfmix_read import read_rfmix_fb, read_rfmix_msp
-from .rfmix_write import write_Q
+from .rfmix_write import write_Q, write_rfmix_fb
 from .ts_read import read_msp_mutations, read_msp_ts
 
 __all__ = [
@@ -9,5 +9,6 @@ __all__ = [
     "write_pgen",
     "read_msp_ts",
     "write_Q",
+    "write_rfmix_fb",
     "read_msp_mutations",
 ]
